5be1ab29c368a19a9c1e966ac7ee249db09c1681
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-22 19:32:02 -0800

    Tie up with firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var rootUrl = 'https://vivid-inferno-7991.firebaseio.com/';
 
-var Hello = React.createClass({
+
+
+var App = React.createClass({
+  mixins: [ReactFire],
+  componentWillMount: function(){
+    this.bindAsObject(new Firebase(rootUrl +'items/'), 'items');
+  },
   render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
+    console.log(this.state);
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+          To-Do List
+        </h2>
+      </div>
+    </div>
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 ReactDOM.render(element, document.querySelector('.container'));
